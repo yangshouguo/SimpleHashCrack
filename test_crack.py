d13442f8c58cdedcaf429960e3ef3bae5b183079
yangshouguo <891584158@qq.com>
2017-04-11 16:24:49 +0800

    Update test_crack.py

--- test_crack.py
+++ test_crack.py
@@ -1,6 +1,7 @@
 import SimpleHashCrack
-addr = 0x400776
-checksum = 3525161884
-length = 6
-crack = SimpleHashCrack.SimpleHashCrash(addr,checksum,length)
+addr = 0x400586 # start address of hash function
+checksum = 367223308 # output of hash function (unsigned int)
+length = 6 #(password length)
+executablefile = 'HelloWorld' # bin file name
+crack = SimpleHashCrack.SimpleHashCrash(addr,checksum,length,executablefile)
 crack.crask()
